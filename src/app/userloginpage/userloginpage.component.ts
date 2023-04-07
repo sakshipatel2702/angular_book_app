3317c43bbd3981f7d08583a9c5f33e00bca52b45
Sakshi Patel <sakshipatel2702@gmail.com>
2023-04-07 16:44:47 -0400

    pushed register

--- src/app/userloginpage/userloginpage.component.ts
+++ src/app/userloginpage/userloginpage.component.ts
@@ -1,23 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-userloginpage',
   templateUrl: './userloginpage.component.html',
   styleUrls: ['./userloginpage.component.css']
 })
-export class UserloginpageComponent {
+export class UserloginpageComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor() {
+  constructor(private Auth: AuthService) {
     this.username = '';
     this.password = '';
   }
 
-  onSubmit() {
-    console.log('Username: ' + this.username);
-    console.log('Password: ' + this.password);
-   
+  ngOnInit() {
+    
   }
+
+  loginUser(event: any) {
+    event.preventDefault()
+    const target = event.target
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+    this.Auth.getUserDetails(username,password)
+    console.log(username, password)
+    //console.log(event)
+     
+  }
+  // username: string;
+  // password: string;
+
+  // constructor() {
+  //   this.username = '';
+  //   this.password = '';
+  // }
+
+  // onSubmit() {
+  //   console.log('Username: ' + this.username);
+  //   console.log('Password: ' + this.password);
+   
+  // }
 }
--- src/app/userregisterpage/userregisterpage.component.ts
+++ src/app/userregisterpage/userregisterpage.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,22 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./userregisterpage.component.css']
 })
 export class UserregisterpageComponent {
-  username: string;
-  password: string;
-  contact: string;
-  birthdate: string;
 
-  constructor() {
-    this.username = '';
-    this.password = '';
-    this.contact = '';
-    this.birthdate = '';
+  username: string="";
+  password: string="";
+  contact: string="";
+  birthdate: string="";
+
+  constructor(private http: HttpClient){
+    
   }
 
-  onSubmit() {
-    console.log('Username: ' + this.username);
-    console.log('Password: ' + this.password);
-    console.log('Contact: ' + this.contact);
-    console.log('Birthdate: ' + this.birthdate);
+  onSubmit(){
+    let bodyData = {
+      "username" : this.username,
+      "password": this.password,
+      "contact": this.contact,
+      "birthdate": this.birthdate
+    };
+    this.http.post("http://localhost:5000/user/create", bodyData, {responseType: 'text'}).subscribe((resultData: any) =>{
+      console.log(resultData);
+      alert("Registered Sucessfully");
+
+      this.username = '';
+      this.password = '';
+      this.contact = '';
+      this.birthdate = '';
+    })
   }
+
+  // username: string;
+  // password: string;
+  // contact: string;
+  // birthdate: string;
+
+  // constructor() {
+  //   this.username = '';
+  //   this.password = '';
+  //   this.contact = '';
+  //   this.birthdate = '';
+  // }
+
+  // onSubmit() {
+  //   console.log('Username: ' + this.username);
+  //   console.log('Password: ' + this.password);
+  //   console.log('Contact: ' + this.contact);
+  //   console.log('Birthdate: ' + this.birthdate);
+  // }
 }
